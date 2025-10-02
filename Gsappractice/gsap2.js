d470a49a3c35d72d65004c5885e2bda82cbd8014
Akshaypratapsingh <2k24.cs1c.2414104@gmail.com>
2025-10-02 14:11:58 +0530

    changes

--- Gsappractice/gsap2.js
+++ Gsappractice/gsap2.js
@@ -1,2 +1,25 @@
-var initial = "M 50 100 Q 600 0 1100 100";
-var final = "M 50 100 Q 600 0 1100 100";
+var path = `M 50 100 Q 600 0 1100 100`;
+var finalpath = `M 50 100 Q 600 100 1100 100`;
+
+var string = document.querySelector("#string");
+
+string.addEventListener("mousemove", function (dets) {
+  path = `M 50 100 Q ${dets.x} ${dets.y} 1100 100`;
+  gsap.to("#string path", {
+    attr: {
+      d: path,
+    },
+    duration: 0.3,
+    ease: "power3.out",
+  });
+});
+
+string.addEventListener("mouseleave", function (dets) {
+  gsap.to("#string path", {
+    attr: {
+      d: finalpath,
+    },
+    duration: 1.5,
+    ease: "elastic.out(1, 0.2)",
+  });
+});
