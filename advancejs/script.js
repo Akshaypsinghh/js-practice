083a14be57cf5a078d7955f45e5947b14d67b05a
Akshaypratapsingh <2k24.cs1c.2414104@gmail.com>
2025-10-03 16:18:29 +0530

    changes

---
+++ advancejs/script.js
@@ -0,0 +1,26 @@
+//lexical and dynamic scoping=>
+// let x = 10;
+// function a() {
+//   console.log(x);
+// }
+// function b() {
+//   let x = 20;
+//   a();
+// }
+// b();
+// closures=>
+// function a() {
+//   let x = 10;
+//   return function () {
+//     console.log(x);
+//   };
+// }
+// let c = a(); //yaha par function execution complete hone par memory stack se hat chuka hai jiske sath sath variable x bhii ..
+
+// c(); //par ye print karega 10.. kyki jab bhi closure banta hai tab unke sath backlink banaya jata hai jiska naam hota hai [[environment]]..
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(1, 2));
