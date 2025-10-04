2523a0d7153117c432bc26d12c363e60b6c74f12
Akshaypratapsingh <2k24.cs1c.2414104@gmail.com>
2025-10-04 16:26:42 +0530

    changes

--- advancejspractice/script.js
+++ advancejspractice/script.js
@@ -1,45 +1,13 @@
-const addfriend = document.querySelector("#addFriend");
-const removefriend = document.querySelector("#removeFriend");
-const input = document.querySelector("#friendName");
-
-let friends = [];
-
-addfriend.addEventListener("click", addfriends);
-removefriend.addEventListener("click", removefriends);
-
-function addfriends() {
-  const name = input.value.trim();
-  if (name == "") {
-    console.log("enter the fucking name bastard!");
-    return;
-  }
-  if (friends.includes(name)) {
-    console.log("friend already exists");
-  } else {
-    friends.push(name);
-    updatelist();
-  }
-  input.value = "";
+function pencil(name, color, price) {
+  this.name = name;
+  this.color = color;
+  this.price = price;
+  this.write = function (text) {
+    let h1 = document.createElement("h1");
+    h1.textContent = text;
+    h1.style.color = color;
+    document.body.append(h1);
+  };
 }
-
-function removefriends() {
-  const name = input.value.trim();
-  const idx = friends.indexOf(name);
-  if (idx == -1) {
-    log("friend not found");
-  } else {
-    friends.splice(idx, 1);
-    updatelist();
-  }
-  input.value = "";
-}
-
-function updatelist() {
-  const list = document.getElementById("friendList");
-  list.innerHTML = "";
-  friends.forEach((friend) => {
-    const li = document.createElement("li");
-    li.textContent = friend;
-    list.appendChild(li);
-  });
-}
+let pencil1 = new pencil("doms", "black", 20);
+let pencil2 = new pencil("natraj", "white", 30);
